1e495bfe234be713134b80e7470b4de7b1c1a124
Anmol <anmol1696@gmail.com>
2022-10-30 17:54:45 +0530

    GH actions tests and action update (#1)
    * Change double quotes to single quotes, and use feature branch for testing UI to avoid too many commits in main
    
    * Fix installation of yq
    
    * Cat yaml file after creating from values
    
    * Add sudo for downloading yq
    
    * Use variables instead of env variables
    
    * Set strings for input values
    
    * Create kind cluster if kubeconfig is empty
    
    * Github path to scripts
    
    * Add debugging section in port forwarding
    
    * Add more test cases
    
    * Update testing for action
    
    * Add a temp kubeconfig file before kind setup
    
    * Fix port forward script, create temp kubeconfig file in pre step
    
    * Fix test yaml
    
    * Fix setting kubeconfig for kind cluster creation
    
    * Add extra log line for testing
    
    * Remove test branch from even branches
    
    * Reduce resources for each chain
    
    * Add deletion of helm chart after testing
    
    * Add name as input for users to provide release name, used to create seperate releases
    
    * Add name to tests action

--- port_forward.sh
+++ port_forward.sh
@@ -9,18 +9,21 @@ echo "num_chains: $num_chains"
 
 echo "Port forwarding all chains genesis to localhost"
 for i in $(seq 0 $(( $num_chains - 1 ))); do
-  chain=$(yq -r ".chains[$i].namea" $VALUES_FILE)
+  chain=$(yq -r ".chains[$i].name" $VALUES_FILE)
   if [[ -z "$chain" || "$chain" == "null" ]]; then
+    echo "Chain not found for index: $i"
     continue
   fi
-  port_rpc=$(yq -r ".chains[$i].ports.rpce" $VALUES_FILE)
+  port_rpc=$(yq -r ".chains[$i].ports.rpc" $VALUES_FILE)
   echo "chain: $chain, port_rpc: $port_rpc"
   if [[ ! -z "$port_rpc" && "$port_rpc" != "null" ]]; then
     kubectl port-forward pods/$chain-genesis-0 $port_rpc:26657 &
+    echo "Port forwarded for pods/$chain-genesis-0 to $port_rpc:26657"
   fi
   port_rest=$(yq -r ".chains[$i].ports.rest" $VALUES_FILE)
   if [[ ! -z "$port_rest" && "$port_rest" != "null" ]]; then
     kubectl port-forward pods/$chain-genesis-0 $port_rest:1317 &
+    echo "Port forwarded for pods/$chain-genesis-0 to $port_rest:1317"
   fi
   echo "chain: $chain, port_rpc: $port_rpc, port_rest: $port_rest"
 done
