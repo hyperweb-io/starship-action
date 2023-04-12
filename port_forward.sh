6aa214a8ef542093b68974f90fb0454f10057566
Anmol1696 <anmol1696@gmail.com>
2023-04-13 00:13:12 +0530

    Update the port forward script

--- port_forward.sh
+++ port_forward.sh
@@ -6,25 +6,53 @@ set -o pipefail
 
 num_chains=$(yq -r ".chains | length" $VALUES_FILE)
 
-echo "num_chains: $num_chains"
+function stop_port_forward() {
+  color 33 "Trying to stop all port-forward, if any...."
+  PIDS=$(ps -ef | grep -i -e 'kubectl port-forward' | grep -v 'grep' | cat | awk '{print $2}') || true
+  for p in $PIDS; do
+    kill -15 $p
+  done
+}
 
-echo "Port forwarding all chains genesis to localhost"
-for i in $(seq 0 $(( $num_chains - 1 ))); do
-  chain=$(yq -r ".chains[$i].name" $VALUES_FILE)
-  if [[ -z "$chain" || "$chain" == "null" ]]; then
-    echo "Chain not found for index: $i"
-    continue
-  fi
-  port_rpc=$(yq -r ".chains[$i].ports.rpc" $VALUES_FILE)
-  echo "chain: $chain, port_rpc: $port_rpc"
-  if [[ ! -z "$port_rpc" && "$port_rpc" != "null" ]]; then
-    kubectl port-forward pods/$chain-genesis-0 $port_rpc:26657 &
-    echo "Port forwarded for pods/$chain-genesis-0 to $port_rpc:26657"
-  fi
-  port_rest=$(yq -r ".chains[$i].ports.rest" $VALUES_FILE)
-  if [[ ! -z "$port_rest" && "$port_rest" != "null" ]]; then
-    kubectl port-forward pods/$chain-genesis-0 $port_rest:1317 &
-    echo "Port forwarded for pods/$chain-genesis-0 to $port_rest:1317"
-  fi
-  echo "chain: $chain, port_rpc: $port_rpc, port_rest: $port_rest"
+# Default values
+CHAIN_RPC_PORT=26657
+CHAIN_LCD_PORT=1317
+CHAIN_EXPOSER_PORT=8081
+EXPLORER_LCD_PORT=8080
+REGISTRY_LCD_PORT=8080
+REGISTRY_GRPC_PORT=9090
+
+stop_port_forward
+
+echo "Port forwarding for config ${VALUES_FILE}"
+echo "Port forwarding all chains"
+num_chains=$(yq -r ".chains | length - 1" ${VALUES_FILE})
+if [[ $num_chains -lt 0 ]]; then
+  echo "No chains to port-forward: num: $num_chains"
+  exit 1
+fi
+for i in $(seq 0 $num_chains); do
+  chain=$(yq -r ".chains[$i].name" ${VALUES_FILE} )
+  localrpc=$(yq -r ".chains[$i].ports.rpc" ${VALUES_FILE} )
+  locallcd=$(yq -r ".chains[$i].ports.rest" ${VALUES_FILE} )
+  localexp=$(yq -r ".chains[$i].ports.exposer" ${VALUES_FILE})
+  [[ "$localrpc" != "null" ]] && kubectl port-forward pods/$chain-genesis-0 $localrpc:$CHAIN_RPC_PORT > /dev/null 2>&1 &
+  [[ "$locallcd" != "null" ]] && kubectl port-forward pods/$chain-genesis-0 $locallcd:$CHAIN_LCD_PORT > /dev/null 2>&1 &
+  [[ "$localexp" != "null" ]] && kubectl port-forward pods/$chain-genesis-0 $localexp:$CHAIN_EXPOSER_PORT > /dev/null 2>&1 &
+  sleep 1
 done
+
+echo "Port forward services"
+
+if [[ $(yq -r ".registry.enabled" $VALUES_FILE) == "true" ]];
+then
+  kubectl port-forward service/registry 8081:$REGISTRY_LCD_PORT > /dev/null 2>&1 &
+  kubectl port-forward service/registry 9091:$REGISTRY_GRPC_PORT > /dev/null 2>&1 &
+  sleep 1
+fi
+
+if [[ $(yq -r ".explorer.enabled" $VALUES_FILE) == "true" ]];
+then
+  kubectl port-forward service/explorer 8080:$EXPLORER_LCD_PORT > /dev/null 2>&1 &
+  sleep 1
+fi
